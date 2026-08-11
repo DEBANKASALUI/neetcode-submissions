a81fb8f14c2887606263b607f667556836e8816e
DEBANKA SALUI <debankasalui@gmail.com>
2026-08-11 11:49:52 +0530

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int maxLen=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+            count=count+1;
+            maxLen=Math.max(maxLen,count);
+            }else count=0;
+        }
+        return maxLen;
+    }
+}
