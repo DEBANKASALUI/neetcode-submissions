9e6c492b0c1edb86fe944e9305db3cb231af3562
DEBANKA SALUI <debankasalui@gmail.com>
2026-08-17 20:13:13 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int max=0;
+       int buy=prices[0];
+       for(int i=1;i<prices.length;i++){
+        if(buy>prices[i]){
+            buy=prices[i];
+        }else{
+            int profit=prices[i]-buy;
+            max=Math.max(max,profit);
+        }
+    }
+       return max; 
+    }
+}
